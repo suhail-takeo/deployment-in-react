911a36332789bb38d866e51989730dd5dbbc9dba
suhailibnnisar <suhailibnnisar@gmail.com>
2021-10-22 08:25:53 +0545

    add logo & v2 text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 
-/*
-
-
-
-*/
-
 function App() {
-  return <div>React App Deployment!!</div>;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>New Website v2 with major changes</h1>
+      <img
+        src="https://www.takeo.ai/wp-content/uploads/2020/06/takeologofulldark-1.png.webp"
+        alt="Logo"
+        style={{
+          width: "40%",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
